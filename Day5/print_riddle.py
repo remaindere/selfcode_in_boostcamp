5da29cb4efd4d94c17aeffe4644917c0c04e3043
remaindere <remaindere@gmail.com>
2021-01-22 22:56:41 +0900

    update riddle codes

---
+++ Day5/print_riddle.py
@@ -0,0 +1,14 @@
+print(0 != 0 not in [1,2,3]) 
+print((0 != 0) not in [1,2,3])
+print(0 != 0 not in [0,1,2,3])
+print((0 != 0) not in [0,1,2,3])
+
+print(bool(0)) #bool False == int value 0
+print(bool(1)) #bool True == other int values 
+print(bool(4)) #True
+print(bool(-1)) #True
+
+print(0 != 0) #False
+print(False not in [1,2,3]) #True
+
+#answer = FTFF
