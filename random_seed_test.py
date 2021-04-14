d55176671e88892789ba83fb3eff637ae84cdf2d
Kwangwon Song <48322490+remaindere@users.noreply.github.com>
2021-04-15 00:08:35 +0900

    Create random_seed_test.py

---
+++ random_seed_test.py
@@ -0,0 +1,11 @@
+import random
+
+if __name__ == '__main__':
+    random.seed(42)
+    x = random.randint(0,2448)
+    print(x)
+    x = random.randint(0,2448)
+    print(x)
+    
+    #결론
+    #random seed 를 
